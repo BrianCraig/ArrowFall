d2374eafadc71f65f5a3ea7d6118cc73633ca554
Brian Craig <briancraigok@gmail.com>
2020-10-28 08:26:20 -0300

    added a lot of things pog pog pog

---
+++ Assets/Helpers/FPSBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSBehaviour : UnityEngine.MonoBehaviour
+{
+    private Text _text;
+    private float _waitTime = 0f;
+    private int _fps = 0;
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 0;
+        }
+        _waitTime -= Time.smoothDeltaTime;
+        _fps++;
+        if (!(_waitTime < 0)) return;
+        _text.text = "FPS: " + _fps;
+        _fps = 0;
+        _waitTime = 1f;
+    }
+}
---
+++ Assets/Scripts/RotateOnCreationBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace ArrowFall
+{
+    public class RotateOnCreationBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+            float GetRandom() => Mathf.Floor(Random.value * 4) * 90;
+            var rotation = Quaternion.identity;
+            rotation *= Quaternion.Euler(GetRandom(), 0f, 0f);
+            rotation *= Quaternion.Euler(0f, GetRandom(), 0f);
+            rotation *= Quaternion.Euler(0f, 0f, GetRandom());
+            transform.rotation = rotation;
+        }
+    }
+}
